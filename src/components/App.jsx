f7b9f82cb046f03cec6056e19f48491707e5f656
Subachandran <jksubi@gmail.com>
2022-12-21 21:07:57 +0530

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Header from "./Header";
-import Note from './Note'
-import Footer from './Footer'
+import React    from "react";
+import Header   from "./Header";
+import Note     from './Note'
+import Footer   from './Footer'
 import allNotes from '../allNotes'
 
 function noteCard(noteInfo, index) {
